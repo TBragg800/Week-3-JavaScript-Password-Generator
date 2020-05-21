cafb9f23dfab5456f935de07b3276f7caa7b52d8
LM&T <tbbragg83@gmail.com>
2020-05-21 15:13:22 -0500

    Start prompt properly operating in function based on user input

--- Develop/script.js
+++ Develop/script.js
@@ -6,9 +6,7 @@ var Upper;
 var Lower;
 var Num;
 var Char;
-//Start = prompt("Please start by selecting a numerical length for your new password between 8 and 128 characters.")
 
-// document.querySelector(generateBtn).addEventListener('click', promptMe);
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -17,7 +15,21 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  
   Start = prompt("Please start by selecting a numerical length for your new password between 8 and 128 characters.");
+  
+    if (!Start) {
+
+      alert("You must enter a value between 8 and 128 into this field!");
+      return;
+
+  } else if  ( Start < 8 || Start > 128){
+
+      alert("You must enter a value between 8 and 128 into this field!");
+      return;
+
+  } 
+  
   Upper = confirm("Do you want to include uppercase letters?");
   Lower = confirm("Do you want to include lowercase letters?");
   Num = confirm("Do you want to include numbers?");
