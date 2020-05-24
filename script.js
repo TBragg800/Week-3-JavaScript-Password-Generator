689abbb5c11da93ef58d692224ffd6494a61f956
LM&T <tbbragg83@gmail.com>
2020-05-23 22:10:34 -0500

    added user input combinations and began on tailoring the function

--- script.js
+++ script.js
@@ -3,12 +3,14 @@
 //start should begin with generate password button
 var Start;
 var Upper;
-var UpperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+UpperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var Lower;
+LowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var Num;
-var NumArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+NumArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var Char;
-var CharArray = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "|", "'", ",", " < ", ".", " > ", "/", " ? "];
+CharArray = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "|", "'", ",", " < ", ".", " > ", "/", " ? "];
+var UserInput;
 //var chars = "abcdefghijklmnopqrstuvwxyz!@#$ %^&*()-+<>ABCDEFGHIJKLMNOP1234567890";
 
 // Assignment Code
@@ -32,14 +34,72 @@ function writePassword() {
       alert("You must enter a value between 8 and 128 into this field!");
       return;
 
-  } 
+  } else {
   
   Upper = confirm("Do you want to include uppercase letters?");
   Lower = confirm("Do you want to include lowercase letters?");
   Num = confirm("Do you want to include numbers?");
   Char = confirm("Do you want to include characters?");
+  }
+
+  //create all combinations of userInput
+  if (Upper && Lower && Num && Char) {
+    UserInput = UpperArray.concat(LowerArray, NumArray, CharArray);
+  } else 
+  if (Upper && Num && Char) {
+    UserInput = UpperArray.concat(NumArray, CharArray);
+  } else 
+  if (Upper && Lower && Char) {
+    UserInput = UpperArray.concat(LowerArray, CharArray);
+  } else
+  if (Upper && Lower && Num) {
+    UserInput = UpperArray.concat(LowerArray, NumArray);
+  } else 
+  if (Lower && Num && Char) {
+    UserInput = LowerArray.concat(NumArray, CharArray);
+  } else 
+  if (Upper && Lower) {
+    UserInput = UpperArray.concat(LowerArray);
+  } else 
+  if (Upper && Num) {
+    UserInput = UpperArray.concat(NumArray);
+  } else 
+  if (Upper && Char) {
+    UserInput = UpperArray.concat(CharArray);
+  } else
+  if (Lower && Num) {
+    UserInput = LowerArray.concat(NumArray);
+  } else
+  if (Lower && Char) {
+    UserInput = LowerArray.concat(CharArray);
+  } else 
+  if (Num && Char) {
+    UserInput = NumArray.concat(CharArray);
+  } else 
+  if (Upper) {
+    UserInput = UpperArray;
+  } else
+  if (Lower) {
+    UserInput = LowerArray;
+  } else 
+  if (Num) {
+    UserInput = NumArray;
+  }
+  if (Char) {
+    UserInput = CharArray;
+  } else {
+    alert("You must hit ok to at least 1 option in order to generate your password!");
+    return;
+  }
+  
 
 
+  console.log(UserInput);
+
+  for ( i = 0; i < Start; i++) {
+    var totalChoices = UserInput[Math.floor(Math.random() * UserInput.length)]
+
+  }
 
   
   var password = generatePassword();
